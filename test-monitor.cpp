1f68082b965b13223963f4d98c3ed0150c0d65b8
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-08-22 10:20:12 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,21 +2,22 @@
 #include "./monitor.h"
 
 TEST(Monitor, TemperatureOutOfRange) {
-    EXPECT_FALSE(vitalsOk(103, 80, 95));
-    EXPECT_FALSE(vitalsOk(94, 80, 95));
+    EXPECT_FALSE(vitalsInRange(103, 80, 95));
+    EXPECT_FALSE(vitalsInRange(94, 80, 95));
 }
 
 TEST(Monitor, PulseRateOutOfRange) {
-    EXPECT_FALSE(vitalsOk(98, 59, 95));
-    EXPECT_FALSE(vitalsOk(98, 101, 95));
+    EXPECT_FALSE(vitalsInRange(98, 59, 95));
+    EXPECT_FALSE(vitalsInRange(98, 101, 95));
 }
 
 TEST(Monitor, Spo2OutOfRange) {
-    EXPECT_FALSE(vitalsOk(98, 80, 89));
+    EXPECT_FALSE(vitalsInRange(98, 80, 89));
 }
 
 TEST(Monitor, AllVitalsOk) {
-    EXPECT_TRUE(vitalsOk(98.1, 70, 98));
-    EXPECT_TRUE(vitalsOk(95, 60, 90));
-    EXPECT_TRUE(vitalsOk(102, 100, 90));
+    EXPECT_TRUE(vitalsInRange(98.1, 70, 98));
+    EXPECT_TRUE(vitalsInRange(95, 60, 90));
+    EXPECT_TRUE(vitalsInRange(102, 100, 90));
 }
+
