c7f0a5fc1232f83941ca80efe10cc582d4019c2e
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-08-22 10:09:00 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,7 +1,8 @@
 #pragma once
 
-bool isTemperatureOk(float temperature);
-bool isPulseRateOk(float pulseRate);
-bool isSpo2Ok(float spo2);
-bool vitalsOk(float temperature, float pulseRate, float spo2);
+bool isTemperatureInRange(float temperature);
+bool isPulseRateInRange(float pulseRate);
+bool isSpoInRange(float spo2);
+bool vitalsInRange(float temperature, float pulseRate, float spo2);
 void alert(const char* message);
+
